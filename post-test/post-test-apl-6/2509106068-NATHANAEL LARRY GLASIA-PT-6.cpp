b308266d8f565b3f3e2cd2a744c0f9df877920db
larrynathanx <nathanaellarry@gmail.com>
2026-04-16 15:46:00 +0800

    pt6 searching

---
+++ post-test/post-test-apl-6/2509106068-NATHANAEL LARRY GLASIA-PT-6.cpp
@@ -0,0 +1,577 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct User
+{
+    string username;
+    string password;
+    string role;
+};
+
+struct StatusKamera
+{
+    string kondisi;
+};
+
+struct Kamera
+{
+    int ID_Kamera;
+    string NamaKamera;
+    int HargaSewa;
+    int LamaSewa;
+    string NamaPenyewa;
+    StatusKamera status;
+};
+
+void RegisterUser(User *user, int &JumlahUser)
+{
+    string username, Password;
+    if (JumlahUser >= 2)
+    {
+        cout << "User penuh";
+        return;
+    }
+    cout << "\n========================\n";
+    cout << "      REGISTER USER\n";
+    cout << "========================\n";
+    cout << "Masukkan nama baru: ";
+    getline(cin, (user + JumlahUser)->username);
+    cout << "Masukkan password baru: ";
+    getline(cin, (user + JumlahUser)->password);
+    (user + JumlahUser)->role = "user";
+    JumlahUser++;
+    cout << "Registrasi berhasil! Silakan login\n";
+}
+
+bool LoginUser(User *user, int &JumlahUser, string &InputNama, string &role_login)
+{
+    string InputPassword;
+    int percobaan_login = 0;
+
+    while (percobaan_login < 3)
+    {
+        cout << "\n===================\n";
+        cout << "       LOGIN\n";
+        cout << "===================\n";
+        cout << "Masukkan username: ";
+        getline(cin, InputNama);
+        cout << "Masukkan password: ";
+        getline(cin, InputPassword);
+
+        for (int i = 0; i < JumlahUser; i++)
+        {
+            if (InputNama == (user + i)->username && InputPassword == (user + i)->password)
+            {
+                cout << "Berhasil login! Selamat datang " << InputNama << endl;
+                role_login = (user + i)->role;
+                return true;
+            }
+        }
+        percobaan_login++;
+        cout << "Gagal login! Sisa percobaan: " << (3 - percobaan_login) << endl;
+    }
+    cout << "Gagal 3x! Akses ditolak!\n";
+    return false;
+}
+
+void TambahDataKamera(Kamera *data, int &JumlahData)
+{
+    if (JumlahData == 10)
+    {
+        cout << "Data kamera penuh!";
+        return;
+    }
+    cout << "\n========================================================================================" << endl;
+    cout << "                          TAMBAH DATA KAMERA TWISTERS CAM                                 " << endl;
+    cout << "========================================================================================" << endl;
+    cout << "ID Kamera: ";
+    cin >> (data + JumlahData)->ID_Kamera;
+    cin.ignore();
+    cout << "Nama Kamera: ";
+    getline(cin, (data + JumlahData)->NamaKamera);
+    cout << "Harga sewa/hari: Rp";
+    cin >> (data + JumlahData)->HargaSewa;
+    cin.ignore();
+    (data + JumlahData)->status.kondisi = "Tersedia";
+    JumlahData++;
+    cout << "Data berhasil ditambahkan";
+    cout << endl;
+}
+
+void TampilkanDataKamera(Kamera *data, int &JumlahData)
+{
+    if (JumlahData == 0)
+    {
+        cout << "Data kamera belum tersedia";
+        return;
+    }
+    cout << "\n========================================================================================\n";
+    cout << "                           DATA KAMERA TWISTERS CAM\n";
+    cout << "========================================================================================\n";
+    cout << left << setw(5) << "No";
+    cout << setw(10) << "ID";
+    cout << setw(20) << "Nama Kamera";
+    cout << setw(20) << "Harga Sewa/hari";
+    cout << setw(12) << "Status";
+    cout << endl;
+    cout << "---------------------------------------------------------------------------------------\n";
+
+    for (int i = 0; i < JumlahData; i++)
+    {
+        cout << left << setw(5) << i + 1;
+        cout << setw(10) << (data + i)->ID_Kamera;
+        cout << setw(20) << (data + i)->NamaKamera;
+        cout << "Rp" << setw(17) << (data + i)->HargaSewa;
+        cout << setw(12) << (data + i)->status.kondisi << endl;
+    }
+    cout << "========================================================================================\n";
+}
+void UbahDataKamera(Kamera *data, int &JumlahData)
+{
+    int index;
+    if (JumlahData == 0)
+    {
+        cout << "Data kamera belum tersedia";
+        return;
+    }
+    TampilkanDataKamera(data, JumlahData);
+    cout << "Pilih nomor kamera yang ingin diubah: ";
+    cin >> index;
+    cin.ignore();
+    index--;
+
+    if (index >= 0 && index < JumlahData)
+    {
+        cout << "ID Kamera baru: ";
+        cin >> (data + index)->ID_Kamera;
+        cin.ignore();
+        cout << "Nama Kamera baru: ";
+        getline(cin, (data + index)->NamaKamera);
+        cout << "Harga sewa/hari: Rp";
+        cin >> (data + index)->HargaSewa;
+        cin.ignore();
+        cout << "Status (Tersedia/Disewa): ";
+        getline(cin, (data + index)->status.kondisi);
+        cout << "Data kamera berhasil diubah!\n";
+        TampilkanDataKamera(data, JumlahData);
+    }
+    else
+    {
+        cout << "Nomor tidak valid!";
+    }
+}
+
+void HapusDataKamera(Kamera *data, int &JumlahData)
+{
+    if (JumlahData == 0)
+    {
+        cout << "Data kamera tidak tersedia!";
+        return;
+    }
+    int index;
+    char konfir_hapus;
+    TampilkanDataKamera(data, JumlahData);
+    cout << "Pilih nomor kamera yang ingin dihapus: ";
+    cin >> index;
+    cin.ignore();
+    index--;
+
+    if (index >= 0 && index < JumlahData)
+    {
+        cout << "Yakin ingin hapus data kamera ini? (Y/N): ";
+        cin >> konfir_hapus;
+
+        if (konfir_hapus == 'Y' || konfir_hapus == 'y')
+        {
+            for (int i = index; i < JumlahData - 1; i++)
+            {
+                *(data + i) = *(data + i + 1);
+            }
+            (JumlahData)--;
+            cout << "Data kamera berhasil dihapus!\n";
+            TampilkanDataKamera(data, JumlahData);
+        }
+        else
+        {
+            cout << "Penghapusan dibatalkan!\n";
+        }
+    }
+    else
+    {
+        cout << "Nomor tidak valid!\n";
+    }
+}
+
+int KembalikanKamera(int &harga, int &hari)
+{
+    if (hari == 0)
+    {
+        return 0;
+    }
+    int sisaHari = hari - 1;
+    return harga + KembalikanKamera(harga, sisaHari);
+}
+
+int KembalikanKamera(int harga, int lamaHari, float diskonSewa)
+{
+    return (harga * lamaHari) - ((harga * lamaHari) * diskonSewa);
+}
+
+void BubbleSortKameraDescending(Kamera *data, int &JumlahData)
+{
+    for (int i = 0; i < JumlahData - 1; i++)
+    {
+        for (int j = 0; j < JumlahData - i - 1; j++)
+        {
+            if ((data + j)->NamaKamera < (data + j + 1)->NamaKamera)
+            {
+                Kamera temp = *(data + j);
+                *(data + j) = *(data + j + 1);
+                *(data + j + 1) = temp;
+            }
+        }
+    }
+}
+
+void SelectionSortHargaAscending(Kamera *data, int &JumlahData)
+{
+    for (int i = 0; i < JumlahData - 1; i++)
+    {
+        int IndexMin = i;
+        for (int j = i + 1; j < JumlahData; j++)
+        {
+            if ((data + j)->HargaSewa < (data + IndexMin)->HargaSewa)
+            {
+                IndexMin = j;
+            }
+        }
+        Kamera temp = *(data + i);
+        *(data + i) = *(data + IndexMin);
+        *(data + IndexMin) = temp;
+    }
+}
+
+int PartitionID(Kamera *data, int low, int high)
+{
+    int pivot = (data + high)->ID_Kamera;
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if ((data + j)->ID_Kamera < pivot)
+        {
+            i++;
+            Kamera temp = *(data + i);
+            *(data + i) = *(data + j);
+            *(data + j) = temp;
+        }
+    }
+
+    Kamera temp = *(data + i + 1);
+    *(data + i + 1) = *(data + high);
+    *(data + high) = temp;
+    return i + 1;
+}
+
+void QuickSortID(Kamera *data, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = PartitionID(data, low, high);
+
+        int kiriLow = low, kiriHigh = pivot - 1, kananLow = pivot + 1, kananHigh = high;
+
+        QuickSortID(data, low, pivot - 1);
+        QuickSortID(data, pivot + 1, high);
+    }
+}
+
+int LinearSearchKamera(Kamera *data, int JumlahData, string key)
+{
+    for (int i = 0; i < JumlahData; i++)
+    {
+        if ((data + i)->NamaKamera == key)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int BinarySearchID(Kamera *data, int JumlahData, int key)
+{
+    int kiri = 0, kanan = JumlahData - 1;
+    while (kiri <= kanan)
+    {
+        int tengah = (kiri + kanan) / 2;
+
+        if ((data + tengah)->ID_Kamera == key)
+        {
+            return tengah;
+        }
+        else if ((data + tengah)->ID_Kamera < key)
+        {
+            kiri = tengah + 1;
+        }
+        else
+        {
+            kanan = tengah - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    Kamera data[10];
+    User user[2];
+    int JumlahUser = 0, JumlahData = 0;
+    user[JumlahUser++] = {"LARRY", "068", "admin"};
+    bool MenuAwal = true;
+
+    while (MenuAwal)
+    {
+        int PilihMenu;
+        string InputNama, role_login;
+
+        cout << "\n==========================\n";
+        cout << "  HOME PAGE TWISTERS CAM\n";
+        cout << "==========================\n";
+        cout << "1. Register\n2. Login\n3. Keluar\n";
+        cout << "Pilih (1-3): ";
+        cin >> PilihMenu;
+        cin.ignore();
+
+        switch (PilihMenu)
+        {
+        case 1:
+            RegisterUser(user, JumlahUser);
+            break;
+
+        case 2:
+            if (!LoginUser(user, JumlahUser, InputNama, role_login))
+            {
+                return 0;
+            }
+            if (role_login == "admin")
+            {
+                int pilihanAdmin;
+                do
+                {
+                    cout << "\n======================\n";
+                    cout << "       MENU ADMIN\n";
+                    cout << "======================\n";
+                    cout << "1. Tambah Data Kamera\n2. Tampilkan Data Kamera\n3. Ubah Data Kamera\n4. Hapus Data Kamera\n5. Sortir Nama Kamera\n6. Sortir Harga Sewa Kamera\n7. Sortir ID Kamera\n8. Search Berdasarkan Nama Kamera\n9. Search Berdasarkan ID Kamera\n10. Logout\n";
+                    cout << "Pilih (1-10): ";
+                    cin >> pilihanAdmin;
+                    cin.ignore();
+
+                    switch (pilihanAdmin)
+                    {
+                    case 1:
+                        TambahDataKamera(data, JumlahData);
+                        break;
+
+                    case 2:
+                        TampilkanDataKamera(data, JumlahData);
+                        break;
+
+                    case 3:
+                        UbahDataKamera(data, JumlahData);
+                        break;
+
+                    case 4:
+                        HapusDataKamera(data, JumlahData);
+                        break;
+
+                    case 5:
+                        BubbleSortKameraDescending(data, JumlahData);
+                        cout << "Sort Nama Kamera berhasil\n";
+                        TampilkanDataKamera(data, JumlahData);
+                        break;
+
+                    case 6:
+                        SelectionSortHargaAscending(data, JumlahData);
+                        cout << "Sort Harga Kamera berhasil\n";
+                        TampilkanDataKamera(data, JumlahData);
+                        break;
+
+                    case 7:
+                        QuickSortID(data, 0, JumlahData - 1);
+                        cout << "Sort ID Kamera berhasil\n";
+                        TampilkanDataKamera(data, JumlahData);
+                        break;
+
+                    case 8:
+                    {
+                        if (JumlahData == 0)
+                        {
+                            cout << "Data Kosong\n";
+                            break;
+                        }
+                        string CariNama;
+                        cout << "Masukkan nama kamera: ";
+                        getline(cin, CariNama);
+                        int hasil = LinearSearchKamera(data, JumlahData, CariNama);
+
+                        if (hasil != -1)
+                        {
+                            cout << "Data kamera ditemukan\n";
+                            cout << "ID Kamera: " << (data + hasil)->ID_Kamera << endl;
+                            cout << "Nama Kamera: " << (data + hasil)->NamaKamera << endl;
+                            cout << "Harga sewa/hari:" << " Rp" << (data + hasil)->HargaSewa << endl;
+                            cout << "Status: " << (data + hasil)->status.kondisi << endl;
+                        }
+                        else
+                        {
+                            cout << "Data kamera tidak ditemukan!";
+                        }
+                        break;
+                    }
+
+                    case 9:
+                    {
+                        if (JumlahData == 0)
+                        {
+                            cout << "Data kamera kosong\n";
+                            break;
+                        }
+                        QuickSortID(data, 0, JumlahData - 1);
+
+                        int CariID;
+                        cout << "Masukkan ID Kamera:\n";
+                        cin >> CariID;
+                        int hasil = BinarySearchID(data, JumlahData, CariID);
+
+                        if (hasil != -1)
+                        {
+                            cout << "Data kamera ditemukan\n";
+                            cout << "ID Kamera: " << (data + hasil)->ID_Kamera << endl;
+                            cout << "Nama Kamera: " << (data + hasil)->NamaKamera << endl;
+                            cout << "Harga sewa/hari:" << " Rp" << (data + hasil)->HargaSewa << endl;
+                            cout << "Status: " << (data + hasil)->status.kondisi << endl;
+                        }
+                        else
+                        {
+                            cout << "Data kamera tidak ditemukan\n";
+                        }
+                        break;
+                    }
+                    case 10:
+                        break;
+                    }
+
+                } while (pilihanAdmin != 10);
+            }
+            else
+            {
+                int pilihanUser;
+                do
+                {
+                    cout << "\n======================\n";
+                    cout << "       MENU USER\n";
+                    cout << "======================\n";
+                    cout << "1. Lihat Data Kamera\n";
+                    cout << "2. Sewa Kamera\n";
+                    cout << "3. Kembalikan Kamera\n";
+                    cout << "4. Logout\n";
+                    cout << "Pilih (1-4): ";
+                    cin >> pilihanUser;
+
+                    switch (pilihanUser)
+                    {
+                    case 1:
+                        TampilkanDataKamera(data, JumlahData);
+                        break;
+
+                    case 2:
+                    {
+                        if (JumlahData == 0)
+                        {
+                            cout << "Data kamera belum ada";
+                            break;
+                        }
+                    }
+
+                        {
+                            TampilkanDataKamera(data, JumlahData);
+                            int nomor;
+                            cout << "Pilih nomor kamera yang ingin disewa: ";
+                            cin >> nomor;
+                            nomor--;
+
+                            if ((data + nomor)->status.kondisi == "Tersedia")
+                            {
+                                cout << "Sewa berapa hari: ";
+                                cin >> (data + nomor)->LamaSewa;
+                                (data + nomor)->status.kondisi = "Disewa";
+                                (data + nomor)->NamaPenyewa = InputNama;
+
+                                cout << "Kamera berhasil disewa!\n";
+                            }
+                            else
+                            {
+                                cout << "Kamera sedang disewa!\n";
+                            }
+                            break;
+                        }
+
+                    case 3:
+                    {
+                        if (JumlahData == 0)
+                        {
+                            cout << "Data kamera belum ada";
+                            break;
+                        }
+                    }
+
+                        {
+                            TampilkanDataKamera(data, JumlahData);
+                            int nomor;
+                            cout << "Pilih nomor kamera yang Anda sewa untuk dikembalikan: ";
+                            cin >> nomor;
+                            nomor--;
+
+                            if (nomor < 0 || nomor >= JumlahData)
+                            {
+                                cout << "Nomor tidak valid\n";
+                                break;
+                            }
+
+                            if ((data + nomor)->NamaPenyewa != InputNama)
+                            {
+                                cout << "Anda tidak menyewa kamera ini\n";
+                                break;
+                            }
+
+                            int total = KembalikanKamera((data + nomor)->HargaSewa, (data + nomor)->LamaSewa);
+
+                            if (total >= 750000)
+                            {
+                                total = KembalikanKamera((data + nomor)->HargaSewa, (data + nomor)->LamaSewa, 0.15);
+                            }
+
+                            cout << "Total harga sewa yang harus dibayar: Rp" << total << endl;
+                            (data + nomor)->status.kondisi = "Tersedia";
+                            (data + nomor)->NamaPenyewa = "";
+                            (data + nomor)->LamaSewa = 0;
+                            cout << "Kamera berhasil dikembalikan\n";
+                            break;
+                        }
+                    }
+                } while (pilihanUser != 4);
+            }
+            break;
+
+        case 3:
+            cout << "Program selesai\n";
+            return 0;
+
+        default:
+            cout << "Pilihan tidak valid\n";
+        }
+    }
+    return 0;
+}
